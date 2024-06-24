b8ced7f081bce40c004598df274e4916481d740f
Verbiturum <user@example.com>
2024-06-24 21:43:57 +0300

    Move components into their own folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './components/App.jsx';
 import './styles/index.css';
 import './styles/reset.css';
 
