87e1d993f84eb85363845657c1480def947ce181
Verbiturum <user@example.com>
2024-07-01 21:44:10 +0300

    Add game logic

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -39,13 +39,36 @@ function Deck() {
 		setCardList(newCardList);
 	}
 
+	function handleCardClick(event) {
+		const clickedImgSrc = event.target.parentElement.firstChild.src;
+
+		let newCardList = cardList;
+		const clickedCard = newCardList.find(card => card.src === clickedImgSrc);
+
+		if (clickedCard.clicked) {
+			// TODO: Implement animation of the card that made you lose the game;	
+			setCurrentScore(0);
+			newCardList = newCardList.map(card => card.clicked = false);
+			setCardList(newCardList);
+		}
+
+		else {
+			clickedCard.clicked = true;
+			setCurrentScore(currentScore + 1);
+			setBestScore(currentScore > bestScore ? currentScore + 1 : bestScore);
+			setCardList(newCardList);
+		}
+
+		shuffleCards();
+	}
+
 	let cardComponentsList = [];
 	cardList.forEach(card => {
 		cardComponentsList.push(<Card key={card.key} src={card.src} description={card.description} />);
 	});
 
 	return (
-		<div className="deck">
+		<div className="deck" onClick={handleCardClick} >
 			<div className="cardsContainer">{cardComponentsList}</div>
 			<div className="infoContainer">
 				<div className="rules">Each time you pick a card, the deck gets shuffled. Pick a card that you didn't choose before to gain a point.</div>
