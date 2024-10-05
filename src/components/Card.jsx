61a988aa7b355784ad705d47503844ea8a948794
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-10-05 16:52:11 +0300

    Escape quote char

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,12 @@
+/* eslint-disable react/prop-types */
 import '../styles/Card.css';
 
 function Card(props) {
-
 	return (
-
 		<div className='card'>
 			<img src={props.src} />
 			<p className='cardDescription'>{props.description}</p>
-			<div className="overlay"></div>
+			<div className='overlay'></div>
 		</div>
 	);
 }
--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -10,7 +10,9 @@ function Deck() {
 
 	useEffect(() => {
 		const newCardList = [];
-		fetch('https://api.artic.edu/api/v1/artworks?limit=12', { mode: 'cors' })
+		fetch('https://api.artic.edu/api/v1/artworks?limit=12', {
+			mode: 'cors',
+		})
 			.then((response) => {
 				return response.json();
 			})
@@ -20,7 +22,7 @@ function Deck() {
 						src: `https://www.artic.edu/iiif/2/${data.data[i].image_id}/full/400,/0/default.jpg`,
 						description: `${data.data[i].title} by ${data.data[i].artist_title}`,
 						key: crypto.randomUUID(),
-						clicked: false
+						clicked: false,
 					});
 				}
 
@@ -44,34 +46,49 @@ function Deck() {
 		const clickedImgSrc = event.target.parentElement.firstChild.src;
 
 		let newCardList = cardList;
-		const clickedCard = newCardList.find(card => card.src === clickedImgSrc);
+		const clickedCard = newCardList.find(
+			(card) => card.src === clickedImgSrc
+		);
 
 		if (clickedCard.clicked) {
-			event.target.parentElement.classList.add('animate__animated', 'animate__wobble');
-			const animationPromise = (() => {
-				return new Promise(resolve => {
-					event.target.parentElement.addEventListener('animationend', () => {
-						event.target.parentElement.classList.remove('animate__animated', 'animate__wobble');
-						resolve();
-					});
+			event.target.parentElement.classList.add(
+				'animate__animated',
+				'animate__wobble'
+			);
+			const animationPromise = () => {
+				return new Promise((resolve) => {
+					event.target.parentElement.addEventListener(
+						'animationend',
+						() => {
+							event.target.parentElement.classList.remove(
+								'animate__animated',
+								'animate__wobble'
+							);
+							resolve();
+						}
+					);
 				});
-			});
+			};
 			await animationPromise().then(() => {
 				setCurrentScore(0);
-				newCardList = newCardList.map(card => card.clicked = false);
+				newCardList = newCardList.map((card) => (card.clicked = false));
 				setCardList(newCardList);
 			});
-		}
-
-		else {
+		} else {
 			clickedCard.clicked = true;
 			setCurrentScore(currentScore + 1);
 			if (currentScore + 1 > bestScore) {
 				setBestScore(currentScore + 1);
 				const bestScoreElement = document.querySelector('.bestScore');
-				bestScoreElement.classList.add('animate__animated', 'animate__tada');
+				bestScoreElement.classList.add(
+					'animate__animated',
+					'animate__tada'
+				);
 				bestScoreElement.addEventListener('animationend', () => {
-					bestScoreElement.classList.remove('animate__animated', 'animate__tada');
+					bestScoreElement.classList.remove(
+						'animate__animated',
+						'animate__tada'
+					);
 				});
 			}
 			setCardList(newCardList);
@@ -81,16 +98,30 @@ function Deck() {
 	}
 
 	let cardComponentsList = [];
-	cardList.forEach(card => {
-		cardComponentsList.push(<Card key={card.key} src={card.src} description={card.description} />);
+	cardList.forEach((card) => {
+		cardComponentsList.push(
+			<Card
+				key={card.key}
+				src={card.src}
+				description={card.description}
+			/>
+		);
 	});
 
 	return (
-		<div className='deck animate__animated animate__backInRight' onClick={handleCardClick} >
+		<div
+			className='deck animate__animated animate__backInRight'
+			onClick={handleCardClick}
+		>
 			<div className='cardsContainer'>{cardComponentsList}</div>
 			<div className='infoContainer'>
-				<div className='rules'>Each time you pick a card, the deck gets shuffled. Pick a card that you didn't choose before to gain a point.</div>
-				<div className='currentScore'>Current score: {currentScore}</div>
+				<div className='rules'>
+					Each time you pick a card, the deck gets shuffled. Pick a
+					card that you didn&apos;t choose before to gain a point.
+				</div>
+				<div className='currentScore'>
+					Current score: {currentScore}
+				</div>
 				<div className='bestScore'>Best score: {bestScore}</div>
 			</div>
 		</div>
